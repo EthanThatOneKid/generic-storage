75f07f193845736461c451f9c510c710a59425fb
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-11-24 00:04:42 -0800

    Separate testing dependencies

--- demos/default/client/client.ts
+++ demos/default/client/client.ts
@@ -5,13 +5,13 @@ import type { DefaultData } from "../../../http/common/default_data.ts";
 export class DefaultClient implements Client<DefaultData> {
   constructor(
     public readonly origin: string,
-    private readonly fetcher: Fetcher = { fetch },
+    private readonly fetcher: Fetcher = { fetch: fetch },
   ) {}
 
-  async get(key: string) {
+  async get(key: string): Promise<DefaultData> {
     const url = new URL(`${this.origin}/${key}`);
     const res = await this.fetcher.fetch(url);
-    return await res.json();
+    return await res.json() as DefaultData;
   }
 
   async set(data: DefaultData) {
@@ -33,7 +33,7 @@ export class DefaultClient implements Client<DefaultData> {
     }
   }
 
-  async list(data?: Partial<DefaultData>) {
+  async list(data?: Partial<DefaultData>): Promise<DefaultData[]> {
     const res = await this.fetcher.fetch(this.origin, {
       method: "GET",
       headers: {
@@ -41,7 +41,7 @@ export class DefaultClient implements Client<DefaultData> {
       },
       body: JSON.stringify(data),
     });
-    return await res.json();
+    return await res.json() as DefaultData[];
   }
 
   async clear() {
--- deps.ts
+++ deps.ts
@@ -1,8 +1,3 @@
 export type { ServeInit } from "https://deno.land/std@0.165.0/http/server.ts";
 export { serve } from "https://deno.land/std@0.165.0/http/server.ts";
 export { parse } from "https://deno.land/std@0.165.0/flags/mod.ts";
-
-export {
-  assertEquals,
-  assertRejects,
-} from "https://deno.land/std@0.165.0/testing/asserts.ts";
